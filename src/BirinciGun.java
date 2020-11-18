5cccb0277f2c951d550686c0e3b94645dab2cf4c
hakankaya960 <hakankaya1558@gmail.com>
2020-11-18 15:30:26 +0300

    message

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,10 +1,11 @@
 
-public class BirinciGün {
+public class BirinciGun {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("11:05 - Birinci Gun");
 		System.out.println("03:22 - ikinci Gun");
+		System.out.println("03:27 - ikinci Gun");
 	}
 
 }
