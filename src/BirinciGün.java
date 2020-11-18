e6b1cae347339f32d6a8a19868e45d7fe18b5ac4
hakankaya960 <74242189+hakankaya960@users.noreply.github.com>
2020-11-18 15:23:46 +0300

    Update BirinciGün.java

--- src/BirinciGün.java
+++ src/BirinciGün.java
@@ -1,9 +1,10 @@
 
-public class BirinciGün {
+public class BirinciGÃ¼n {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("11:05 - Birinci Gün");
+		System.out.println("11:05 - Birinci Gun");
+		System.out.println("03:22 - ikinci Gun");
 	}
 
 }
